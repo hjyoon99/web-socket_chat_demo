cf8b9162a1178bb4dea8d9d3e27c2f7428fe17f0
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2023-08-11 12:02:16 +0900

    ??

--- src/main/java/com/example/web/socketdemo/WebSocketConfig.java
+++ src/main/java/com/example/web/socketdemo/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.web.socketdemo;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -7,6 +8,7 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     private final WebSocketHandler webSocketHandler;
@@ -16,6 +18,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/chat").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
     }
 }
