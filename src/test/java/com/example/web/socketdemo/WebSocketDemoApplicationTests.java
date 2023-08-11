1570e4614ba01484bfc9795b05f2c313d9f434d3
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2023-08-11 10:42:43 +0900

    Initial commit

---
+++ src/test/java/com/example/web/socketdemo/WebSocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web.socketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
